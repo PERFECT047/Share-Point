7ff4c6acfa04be14596d94a8b6a1c9e50b26c852
PERFECT047 <siddhartha.das.min19@itbhu.ac.in>
2025-01-03 00:45:55 +0530

    Backend_Signin_Done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { sign, verify } from 'hono/jwt'
+import { decode, sign, verify } from 'hono/jwt'
 
 const app = new Hono<{
   Bindings: {
@@ -46,28 +46,51 @@ app.post('/api/v1/user/signup', async (c) => {
   
     const token = await sign({id: user.id, email: user.email}, c.env.JWT_SECRET);
   
-    return c.json({jwt: token})
+    return c.json({jwt: token});
   }
   catch(e){
     c.status(403);
-    return c.json({msg: "user already exists"})
+    return c.json({msg: "user already exists"});
   }
   
+})
+
+app.post('/api/v1/user/signin', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  const body = await c.req.json();
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+      password: body.password
+    }
+  });
+
+  if(!user){
+    c.status(403);
+    return c.json({msg: "user doesn't exist"});
+  }
+
+  const token = sign({id: user.id, email: user.email}, c.env.JWT_SECRET);
+  return c.json({jwt: token});
+
 })
 
 app.get('/api/v1/blog/:id', (c) => {
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate())
+
+  
+
   return c.text('Hello from get blog')
 })
 
 app.get('/api/v1/blog/bulk', (c) => {
   return c.text('Hello from get bulk')
-})
-
-app.post('/api/v1/user/signin', (c) => {
-  return c.text('Hello from post signin')
 })
 
 app.post('/api/v1/blog', (c) => {
